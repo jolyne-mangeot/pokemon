d8f0e4c2cde88a18e30e07c797813e59c587f00c
jolyne-mangeot <jolyne.mangeot@gmail.com>
2025-02-17 12:37:53 +0100

    chore: add sound files and folders for coming feature

---
+++ assets/sfx/README.md
@@ -0,0 +1,1 @@
+for musics and ui sounds
--- main.py
+++ main.py
@@ -9,31 +9,6 @@ from states.in_game_states.new_game import New_game
 from states.in_game_states.launch_menu import Launch_menu
 from states.in_game_states.in_fight import In_fight
 
-# import tracemalloc, linecache
-# def display_top(snapshot, key_type='lineno', limit=10):
-#     snapshot = snapshot.filter_traces((
-#         tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-#         tracemalloc.Filter(False, "<unknown>"),
-#     ))
-#     top_stats = snapshot.statistics(key_type)
-
-#     print("Top %s lines" % limit)
-#     for index, stat in enumerate(top_stats[:limit], 1):
-#         frame = stat.traceback[0]
-#         print("#%s: %s:%s: %.1f KiB"
-#               % (index, frame.filename, frame.lineno, stat.size / 1024))
-#         line = linecache.getline(frame.filename, frame.lineno).strip()
-#         if line:
-#             print('    %s' % line)
-
-#     other = top_stats[limit:]
-#     if other:
-#         size = sum(stat.size for stat in other)
-#         print("%s other: %.1f KiB" % (len(other), size / 1024))
-#     total = sum(stat.size for stat in top_stats)
-#     print("Total allocated size: %.1f KiB" % (total / 1024))
-# tracemalloc.start()
-
 pg.init()
 game = Control()
 game.init_settings()
@@ -48,20 +23,8 @@ STATE_DICT = {
     "launch_menu" : Launch_menu()
 }
 
-# snapshot1 = tracemalloc.take_snapshot()
-# STATE_DICT['load_menu'] = Load_menu()
-# STATE_DICT['game'] = Game()
-
-# STATE_DICT['pause_menu'] = Pause_menu()
-# snapshot2 = tracemalloc.take_snapshot()
-
 game.setup_states(STATE_DICT, "title_menu")
 game.main_game_loop()
-
-# snapshot3 = tracemalloc.take_snapshot()
-# display_top(snapshot1)
-# display_top(snapshot2)
-# display_top(snapshot3)
 
 pg.quit()
 exit()
